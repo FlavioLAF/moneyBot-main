bf6eae3ba25269b7c97e071f63c7a7d4cb8bd228
FlavioLAF <flavio.lucio.a.filho@gmail.com>
2022-04-29 07:40:46 -0300

    main

---
+++ exec_order.py
@@ -0,0 +1,247 @@
+
+import os
+import configparser
+
+from datetime import datetime
+from iqoptionapi.stable_api import IQ_Option
+
+import logging
+logging.disable(level=(logging.DEBUG))
+
+config = configparser.RawConfigParser()
+config.read('C:\\Users\\User\\Desktop\\Dev\\moneyBot-main\\config.cfg')
+_dict_config = dict(config.items('CONFIG'))
+
+amount = float(_dict_config['aporte'])
+_esperar_usuario_fechar = _dict_config['esperar_usuario_fechar']
+_arquivo_sinais = open('C:\\Users\\User\\Desktop\\Dev\\moneyBot-main\\ListaSinais.txt','r')
+
+api = IQ_Option(_dict_config['usuario'], _dict_config['senha'])
+_conect_api = api.connect()
+api.change_balance(_dict_config['tipo_de_conta'])
+
+lista_arquivo = _arquivo_sinais.read() 
+
+lista = lista_arquivo.split('\n')
+
+#_______________________________SUB CLASSES INICIO_________________________________
+class func:
+    def bol_ativo_fechado(_id):
+        try:
+            if 'code' in _id:
+                print('ATIVO FECHADO!')
+                bol_esperar_usuario_fechar()
+            else:
+                pass    
+        except TypeError:
+            pass
+        
+#_______________________________SUB CLASSES FIM___________________________________
+
+#___________________________FUNÇÕES-INICIO________________________________________
+
+def bol_esperar_usuario_fechar():
+    if _esperar_usuario_fechar == 'N' or _esperar_usuario_fechar == 'n':
+        exit()    
+    elif(_esperar_usuario_fechar == 'S' or _esperar_usuario_fechar == 'S'):
+        os.system('pause')
+    else:
+        print('Valor da chave: esperar_usuario_fechar, esta incorreta (só S ou N), por favor, corrija!')
+        os.system('pause')
+
+def bol_ativo_fechado(_id, _sinal):
+    try:
+        if 'code' in _id:
+            print('ATIVO FECHADO: ',_sinal[1])
+            bol_esperar_usuario_fechar()
+        else:
+            pass    
+    except TypeError:
+        pass
+    
+def hora_minuto(lista):
+    for index, percorre in enumerate(lista):
+        _horario = lista[index].split(';')
+
+    _hora_mim = _horario[2].split(':')
+
+    hora_mim = _hora_mim[0] +':'+ _hora_mim[1]
+    
+    return hora_mim
+
+def time_frame(time):
+    if time == 'M1' or time == 'm1':
+        return 1
+    if time == 'M5' or time == 'm5':
+        return 5
+    if time == 'M15' or time == 'm15':
+        return 15
+    if time == 'H1' or time == 'H1':
+        return 60
+
+def resultado(_id):
+    func.bol_ativo_fechado(_id)
+    if _id != "error":
+        while True:
+            ck, win = api.check_win_digital_v2(_id)
+            
+            if ck == True:  
+                break                  
+        return win
+
+def sistema_sem_gale():
+    while hora_minuto(lista) >= datetime.today().strftime('%H:%M'):
+        
+        for index, id in enumerate(lista):
+            _sinal = lista[index].split(';')     
+                    
+            _hora_minuto = _sinal[2].split(':')
+            
+            hora_minutos = _hora_minuto[0] +':'+_hora_minuto[1]
+            
+            if hora_minutos == datetime.today().strftime('%H:%M'):
+                
+                bol, _id = api.buy_digital_spot(_sinal[1], amount, _sinal[3], time_frame(_sinal[0]))
+                            
+                bol_ativo_fechado(_id, _sinal)
+                print('---------------ORDEM EXECUTADA----------------')
+                print('--------------'+lista[index]+'----------------')
+                print('---------------VERIFICANDO WIN----------------')        
+                
+                _res = resultado(_id)
+                if _res < 0:
+                    print('Loss: ', _res)
+                else: 
+                    print('Gain: ', _res)
+            
+                lista[index] = 'XX;XXXXXX;XX:XX:XX;XXX'
+                print(lista[index])
+                print('---------------ORDEM FINALIZADA----------------', index)
+                print('\n \n')      
+    else:
+        print('============================================================')
+        print('__________________SISTEMA FINALIZADO________________________')
+        print('============================================================')
+        bol_esperar_usuario_fechar()
+            
+def sistema_com_um_gale():
+    while hora_minuto(lista) >= datetime.today().strftime('%H:%M'):
+        
+        for index, id in enumerate(lista):
+            _sinal = lista[index].split(';')     
+                    
+            _hora_minuto = _sinal[2].split(':')
+            
+            hora_minutos = _hora_minuto[0] +':'+_hora_minuto[1]
+            
+            if hora_minutos == datetime.today().strftime('%H:%M'):
+                
+                bol, _id = api.buy_digital_spot(_sinal[1], amount, _sinal[3], time_frame(_sinal[0]))
+                            
+                bol_ativo_fechado(_id,_sinal) 
+                print('---------------ORDEM EXECUTADA----------------')
+                print('--------------'+lista[index]+'----------------')
+                print('---------------VERIFICANDO WIN----------------')        
+                if _id != "error":
+                    while True:
+                        ck, win = api.check_win_digital_v2(_id)
+                        
+                        if ck == True:  
+                            break                  
+                    if win < 0:
+                        print('---------------LOSS de '+str(win)+', EXECUTANDO GALE 1----------------')
+                        gale1 = amount * 2.2
+                        _bol ,_id = api.buy_digital_spot(_sinal[1], gale1, _sinal[3], time_frame(_sinal[0]))
+                        print('---------------VERIFICANDO GALE 1----------------')
+                        
+                        bol_ativo_fechado(_id, _sinal)
+                        if _id != "error" :
+                            while True:
+                                ck, win = api.check_win_digital_v2(_id)
+
+                                if ck == True:  
+                                    break
+                                if win < 0:
+                                    print('_____________________HIT: -'+abs(amount+gale1)+'________________________')
+                                else:
+                                    print("Você ganhou "+str(win)+"$ no GALE 1")                   
+                    else:
+                        print("Você ganhou: "+str(win)+"$")
+                lista[index] = 'XX;XXXXXX;XX:XX:XX;XXX'
+                print('---------------ORDEM FINALIZADA----------------')
+                print('\n \n')
+    else:
+        print('============================================================')
+        print('__________________SISTEMA FINALIZADO________________________')
+        print('============================================================')
+        os.system("pause")
+        
+def sistema_com_dois_gales():
+    while hora_minuto(lista) >= datetime.today().strftime('%H:%M'):
+        
+        for index, id in enumerate(lista):
+            _sinal = lista[index].split(';')     
+                    
+            _hora_minuto = _sinal[2].split(':')
+            
+            hora_minuto = _hora_minuto[0] +':'+_hora_minuto[1]
+            
+            if hora_minuto == datetime.today().strftime('%H:%M'):
+                
+                bol, _id = api.buy_digital_spot(_sinal[1], amount, _sinal[3], time_frame(_sinal[0]))
+                            
+                bol_ativo_fechado() 
+                print('---------------ORDEM EXECUTADA----------------')
+                print('--------------'+lista[index]+'----------------')
+                print('---------------VERIFICANDO WIN----------------')        
+                if _id != "error":
+                    while True:
+                        ck, win = api.check_win_digital_v2(_id)
+                        
+                        if ck == True:  
+                            break                  
+                    if win < 0:
+                        print('---------------LOSS de '+str(win)+', EXECUTANDO GALE 1----------------')
+                        gale1 = amount * 2.2
+                        _bol ,_id = api.buy_digital_spot(_sinal[1], gale1, _sinal[3], time_frame(_sinal[0]))
+                        print('---------------VERIFICANDO GALE 1----------------')
+                        bol_ativo_fechado()
+                        if _id != "error":
+                            while True:
+                                ck, win = api.check_win_digital_v2(_id)
+                                
+                                if ck == True:  
+                                    break                  
+                            if win < 0:
+                                print('---------------LOSS de '+str(win)+', EXECUTANDO GALE 2----------------')
+                                gale2 = amount * 2.2
+                                _bol ,_id = api.buy_digital_spot(_sinal[1], gale2, _sinal[3], time_frame(_sinal[0]))
+                                print('---------------VERIFICANDO GALE 2----------------')
+                                bol_ativo_fechado()
+                                if _id != "error":
+                                    while True:
+                                        ck, win = api.check_win_digital_v2(_id)
+                                        
+                                        if ck == True:  
+                                            break                  
+                                    if win < 0:
+                                        print('_____________________HIT: -'+abs(amount+gale1+gale2)+'________________________')
+                                    else:
+                                        print("Você ganhou "+str(win)+"$ no GALE 2")
+                            else:
+                                print("Você ganhou "+str(win)+"$ no GALE 1")
+                    else:
+                        print("Você ganhou "+str(win)+"$")
+            
+        lista[index] = 'XX;XXXXXX;XX:XX:XX;XXX'
+        print('---------------ORDEM FINALIZADA----------------')
+        print('\n \n')
+    else:
+        print('============================================================')
+        print('__________________SISTEMA FINALIZADO________________________')
+        print('============================================================')
+        os.system("pause")
+        
+#___________________________FUNÇÕES-FIM________________________________________
+
+sistema_com_um_gale()
---
+++ separar arquivos.py
@@ -0,0 +1,132 @@
+EURUSD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\EURUSD.txt'
+EURGBP ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\EURGBP.txt'
+GBPJPY ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\GBPJPY.txt'
+EURJPY ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\EURJPY.txt'
+GBPUSD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\GBPUSD.txt'
+USDJPY ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\USDJPY.txt'
+AUDCAD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\AUDCAD.txt'
+NZDUSD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\NZDUSD.txt'
+USDRUB ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\USDRUB.txt'
+USDCHF ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\USDCHF.txt'
+XAUUSD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\XAUUSD.txt'
+XAGUSD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\XAGUSD.txt'
+AUDUSD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\AUDUSD.txt'
+USDCAD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\USDCAD.txt'
+AUDJPY ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\AUDJPY.txt'
+GBPCAD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\GBPCAD.txt'
+GBPCHF ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\GBPCHF.txt'
+GBPAUD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\GBPAUD.txt'
+EURCAD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\EURCAD.txt'
+CHFJPY ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\CHFJPY.txt'
+CADCHF ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\CADCHF.txt'
+EURAUD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\EURAUD.txt'
+USDNOK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\USDNOK.txt'
+EURNZD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\EURNZD.txt'
+USDSEK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\USDSEK.txt'
+USDTRY ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\USDTRY.txt'
+AUDCHF ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\AUDCHF.txt'
+AUDNZD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\AUDNZD.txt'
+CADJPY ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\CADJPY.txt'
+EURCHF ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\EURCHF.txt'
+GBPNZD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\GBPNZD.txt'
+NZDCAD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\NZDCAD.txt'
+NZDJPY ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\NZDJPY.txt'
+EURSEK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\EURSEK.txt'
+EURNOK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\EURNOK.txt'
+CHFSGD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\CHFSGD.txt'
+EURSGD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\EURSGD.txt'
+USDMXN ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\USDMXN.txt'
+AUDDKK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\AUDDKK.txt'
+AUDMXN ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\AUDMXN.txt'
+AUDNOK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\AUDNOK.txt'
+AUDSEK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\AUDSEK.txt'
+AUDSGD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\AUDSGD.txt'
+AUDTRY ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\AUDTRY.txt'
+CADMXN ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\CADMXN.txt'
+CADNOK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\CADNOK.txt'
+CADPLN ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\CADPLN.txt'
+CADTRY ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\CADTRY.txt'
+CHFDKK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\CHFDKK.txt'
+CHFNOK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\CHFNOK.txt'
+CHFSEK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\CHFSEK.txt'
+CHFTRY ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\CHFTRY.txt'
+DKKPLN ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\DKKPLN.txt'
+DKKSGD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\DKKSGD.txt'
+EURDKK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\EURDKK.txt'
+EURMXN ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\EURMXN.txt'
+EURTRY ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\EURTRY.txt'
+EURZAR ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\EURZAR.txt'
+GBPILS ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\GBPILS.txt'
+GBPMXN ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\GBPMXN.txt'
+GBPNOK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\GBPNOK.txt'
+GBPPLN ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\GBPPLN.txt'
+GBPSEK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\GBPSEK.txt'
+GBPSGD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\GBPSGD.txt'
+GBPTRY ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\GBPTRY.txt'
+NOKDKK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\NOKDKK.txt'
+NOKJPY ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\NOKJPY.txt'
+NOKSEK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\NOKSEK.txt'
+NZDDKK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\NZDDKK.txt'
+NZDMXN ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\NZDMXN.txt'
+NZDNOK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\NZDNOK.txt'
+NZDSEK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\NZDSEK.txt'
+NZDSGD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\NZDSGD.txt'
+NZDTRY ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\NZDTRY.txt'
+NZDZAR ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\NZDZAR.txt'
+PLNSEK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\PLNSEK.txt'
+SEKDKK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\SEKDKK.txt'
+SEKJPY ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\SEKJPY.txt'
+SGDJPY ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\SGDJPY.txt'
+USDDKK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\USDDKK.txt'
+NZDCHF ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\NZDCHF.txt'
+GBPHUF ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\GBPHUF.txt'
+USDCZK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\USDCZK.txt'
+USDHUF ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\USDHUF.txt'
+CADSGD ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\CADSGD.txt'
+EURCZK ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\EURCZK.txt'
+EURHUF ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\EURHUF.txt'
+USDTHB ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\USDTHB.txt'
+USDXOF_OTC ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\USDXOF-OTC.txt'
+USDZAR_OTC ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\USDZAR-OTC.txt'
+USDSGD_OTC ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\USDSGD-OTC.txt'
+USDHKD_OTC ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\USDHKD-OTC.txt'
+USDINR_OTC ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\USDINR-OTC.txt'
+EURUSD_OTC ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\EURUSD-OTC.txt'
+EURGBP_OTC ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\EURGBP-OTC.txt'
+USDCHF_OTC ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\USDCHF-OTC.txt'
+EURJPY_OTC ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\EURJPY-OTC.txt'
+NZDUSD_OTC ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\NZDUSD-OTC.txt'
+GBPUSD_OTC ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\GBPUSD-OTC.txt'
+GBPJPY_OTC ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\GBPJPY-OTC.txt'
+USDJPY_OTC ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\USDJPY-OTC.txt'
+AUDCAD_OTC ='C:\Users\User\Desktop\Dev\moneyBot-main\listas\AUDCAD-OTC.txt'
+
+
+    
+    
+    
+
+#for index, row in enumerate(lista_sinais):
+    
+#    _row = lista_sinais[index].split(';')
+    
+#    if _row[1] == 'EURUSD-OTC' or _row[1] == 'EURUSD':
+        #_eurusd = open(EURUSD, 'a')
+        #_eurusd.write(row+'\n')
+        #_eurusd.close()
+    #if _row[1] == 'AUDCAD-OTC' or _row[1] == 'AUDCAD':
+        #_eurusd = open(EURUSD, 'a')
+        #_eurusd.write(row+'\n')
+        #_eurusd.close()
+    #if _row[1] == 'EURBGP-OTC' or _row[1] == 'EURBGP':
+        #_eurusd = open(EURUSD, 'a')
+        #_eurusd.write(row+'\n')
+        #_eurusd.close()
+    #if _row[1] == 'EURJPY-OTC' or _row[1] == 'EURJPY':
+        #_eurusd = open(EURUSD, 'a')
+       # _eurusd.write(row+'\n')
+       # _eurusd.close()
+    #if _row[1] == 'GBPJPY-OTC' or _row[1] == 'GBPJPY':
+       # _eurusd = open(EURUSD, 'a')
+      #  _eurusd.write(row+'\n')
+     #   _eurusd.close()
